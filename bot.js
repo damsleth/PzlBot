5e4dac3df6e7270f70890afe49fcb084c5821f89
Kim Damsleth <kim@damsleth.com>
2016-06-22 17:04:51 +0200

    more bot stuff

--- bot.js
+++ bot.js
@@ -36,7 +36,7 @@ controller.setupWebserver(process.env.PORT || 3001, function(err, webserver) {
 //bot commands
 //===
 
-controller.hears(['hello','hi','hey','yo'],['direct_message','direct_mention','mention'],function(bot,message) {
+controller.hears(['hello','hi','hey'],['direct_message','direct_mention','mention'],function(bot,message) {
     bot.reply(message,"Hi!");
 });
 
@@ -131,8 +131,8 @@ controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your na
         var uptime = formatUptime(process.uptime());
 
         bot.reply(message,
-            ':robot_face: I am a bot named <@' + bot.identity.name +
-             '>. I have been running for ' + uptime + ' on ' + hostname + '.');
+            "I'm <@" + bot.identity.name + ", bitch!",
+             ">. Ive been running for " + uptime + ".");
 
     });
 
