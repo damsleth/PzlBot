fd7795a35c4baf7a7f5f9f3a74c8f2bab69b9f6a
Kim Damsleth <kim@damsleth.com>
2016-12-02 10:01:07 +0100

    moar dice fix

--- bot.js
+++ bot.js
@@ -271,11 +271,9 @@ controller.hears("slap (.*)", ['ambient','direct_mention','mention'], function (
 
 //Throw two Dice
 controller.hears(["two dices", "craps"], ["ambient", "direct_message", "mention", "direct_mention"], function (bot, message) {
-    var dice1 = Math.floor(6 * Math.random());
-    var dice2 = Math.floor(6 * Math.random());
+    var dice1 = Math.floor(6 * Math.random()+1);
+    var dice2 = Math.floor(6 * Math.random()+1);
     var name = helpers.craps(dice1, dice2);
-    dice1++;
-    dice2++;
     var total = dice1 + dice2;
     bot.reply(message, "CRAPS: <@" + message.user + ">, you threw " + dice1 + " and " + dice2 + " for a total of " + total + ". " + helpers.craps(dice1, dice2).toUpperCase());
 });
