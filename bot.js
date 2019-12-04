83d2af107bf1018961fce61103f46d457d8e69fd
Kim Damsleth <kim@damsleth.com>
2019-12-04 11:44:52 +0100

    team details

--- bot.js
+++ bot.js
@@ -316,7 +316,7 @@ controller.hears("batteries", __config.Listeners.All, (bot, message) => bot.repl
 controller.hears(["tacocat", "taco cat", "TACOCAT", "TACO CAT"], __config.Listeners.All, (bot, message) => bot.reply(message, ":taco: :smile_cat:  *_TACOCAT_*  :smile_cat: :taco:"));
 
 // kode24 Julekalender 2019 
-controller.hears(["jul2019", "kodekalender","kode24"], __config.Listeners.NonAmbient, (bot, message) => jul2019.getTeams(bot, message));
+controller.hears(["jul2019 (.*) (.*)", "kodekalender (.*) (.*)","kode24 (.*) (.*)"], __config.Listeners.NonAmbient, (bot, message) => jul2019.getTeams(bot, message));
 
 //OLJEFONDET
 controller.hears(["oljefondet", "nbim", "cash money", "how rich am i", "pensjonsfondet"], __config.Listeners.All, (bot, message) => helpers.nbim(bot, message));
